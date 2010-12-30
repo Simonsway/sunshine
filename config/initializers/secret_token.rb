9410d483889aed7aff40a2fd1c05c22b70d91a8e
Simonsway <simon.eiler@simonsway.com>
2010-12-30 20:24:38 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunshine::Application.config.secret_token = '5f66570f84dbe745ea0b8d0790b0ffacd7bbc0a93a2ac753a9f5cecc329c0f3b0ac1e6a63e0dc96f32440bec62dddc7971b12df925fe71714e052a671a105ae6'
